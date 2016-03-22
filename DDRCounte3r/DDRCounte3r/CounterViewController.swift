fca78635eb65f74e3096531d51eaea3ca3269717
Bobby <rhieger@nyc.rr.com>
2016-03-22 19:41:31 -0400

    Created CounterViewController with Constraints

---
+++ DDRCounte3r/DDRCounte3r/CounterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CounterViewController.swift
+//  DDRCounter
+//
+//  Created by Robert Hieger on 3/22/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
